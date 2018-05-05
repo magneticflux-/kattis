9c8f27328c2e141dabc1592b38e987eaf78c2c29
Mitchell Skaggs <skaggsm333@gmail.com>
2018-05-05 11:39:11 -0500

    Added helpaphd

---
+++ src/main/scala/org/skaggsm/kattis/helpaphd.scala
@@ -0,0 +1,21 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 5/5/2018.
+  */
+object helpaphd {
+  def main(args: Array[String]): Unit = {
+    val numProblems = StdIn.readInt
+
+    (0 until numProblems).foreach(_ => {
+      val line = StdIn.readLine
+
+      if (line == "P=NP")
+        println("skipped")
+      else
+        println(line.split('+').map(_.toInt).sum)
+    })
+  }
+}
