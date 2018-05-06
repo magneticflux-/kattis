2e57599ca9ffad32f8d876201e075dc92b55b9f5
Mitchell Skaggs <skaggsm333@gmail.com>
2018-05-05 19:06:16 -0500

    Added railroad2

---
+++ src/main/scala/org/skaggsm/kattis/railroad2.scala
@@ -0,0 +1,14 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 5/5/2018.
+  */
+object railroad2 {
+  def main(args: Array[String]): Unit = {
+    val Array(_, y) = StdIn.readLine.split(' ').map(_.toInt)
+
+    println(if (y % 2 == 0) "possible" else "impossible")
+  }
+}
