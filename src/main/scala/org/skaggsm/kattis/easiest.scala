8b822a8480a7b026b30af6f047b31cd96e4e9f62
Mitchell Skaggs <skaggsm333@gmail.com>
2018-05-08 17:05:53 -0500

    Added easiest

---
+++ src/main/scala/org/skaggsm/kattis/easiest.scala
@@ -0,0 +1,23 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 5/8/2018.
+  */
+object easiest {
+  def main(args: Array[String]): Unit = {
+    var line = StdIn.readLine
+
+    while (line != null && line != "0") {
+      val n = line.toInt
+      val sum = sumDigits(n)
+
+      (11 to 100).find(i => sum == sumDigits(i * n)) foreach println
+
+      line = StdIn.readLine
+    }
+  }
+
+  def sumDigits(int: Int): Int = int.toString.map(_ - '0').sum
+}
