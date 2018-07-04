1d3df0216466b3efab7dc7a99ed1182f2d15a2a2
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-04 09:57:04 -0500

    Solved stararrangements

---
+++ src/main/scala/org/skaggsm/kattis/stararrangements.scala
@@ -0,0 +1,31 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+import scala.language.postfixOps
+
+/**
+  * Created by Mitchell Skaggs on 7/4/2018.
+  */
+object stararrangements {
+  def main(args: Array[String]): Unit = {
+    val numStars = StdIn.readInt
+
+    println(s"$numStars:")
+
+    val maxRowSize = (numStars + 1) / 2
+
+    val possibilities =
+      (for (x <- 2 to maxRowSize view;
+            y <- x - 1 to x view)
+        yield (x, y))
+        .view
+        // Must sum to the total
+        .filter(p => {
+        numStars % (p._1 + p._2) == 0 ||
+          (p._1 == p._2 && numStars % p._1 == 0) ||
+          ((numStars - p._1) % (p._1 + p._2) == 0)
+      })
+
+    possibilities.foreach(p => println(s"${p._1},${p._2}"))
+  }
+}
