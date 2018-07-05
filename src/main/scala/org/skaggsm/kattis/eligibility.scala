2aea87c492eaf9a27600ab2d8888d312cfcbc423
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-05 15:55:41 -0500

    Solved eligibility

---
+++ src/main/scala/org/skaggsm/kattis/eligibility.scala
@@ -0,0 +1,35 @@
+package org.skaggsm.kattis
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/5/2018.
+  */
+object eligibility {
+  final val DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+
+  def main(args: Array[String]): Unit = {
+    val numContestants = StdIn.readInt
+
+    Iterator.continually(StdIn.readLine)
+      .take(numContestants)
+      .map(line => {
+        val Array(name, studyDateString, birthDateString, courseNumberString) = line.split(' ')
+
+        val studyDate = DATE_FORMATTER.parse[LocalDate](studyDateString, LocalDate.from _)
+        val birthDate = DATE_FORMATTER.parse[LocalDate](birthDateString, LocalDate.from _)
+        val courses = courseNumberString.toInt
+
+        val action =
+          if (studyDate.getYear >= 2010 || birthDate.getYear >= 1991) "eligible"
+          else if (courses > 40) "ineligible"
+          else "coach petitions"
+
+        s"$name $action"
+      })
+      .foreach(println)
+  }
+}
