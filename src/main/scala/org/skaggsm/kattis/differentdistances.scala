827dba9d89d3ab59d2553800634ce50687f7532c
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-05 12:12:26 -0500

    Solved differentdistances

---
+++ src/main/scala/org/skaggsm/kattis/differentdistances.scala
@@ -0,0 +1,19 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+import scala.math.{abs, pow}
+
+/**
+  * Created by Mitchell Skaggs on 7/5/2018.
+  */
+object differentdistances {
+  def main(args: Array[String]): Unit = {
+    Iterator.continually(StdIn.readLine)
+      .takeWhile(_ != "0")
+      .foreach(line => {
+        val Array(x1, y1, x2, y2, p) = line.split(' ').map(_.toDouble)
+
+        println(pow(pow(abs(x1 - x2), p) + pow(abs(y1 - y2), p), 1 / p))
+      })
+  }
+}
