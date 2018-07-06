ae172c5ff18fc5340905da40582e3cd0a1d727e1
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-06 11:36:28 -0500

    Solved onechicken

---
+++ src/main/scala/org/skaggsm/kattis/onechicken.scala
@@ -0,0 +1,19 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/6/2018.
+  */
+object onechicken {
+  def main(args: Array[String]): Unit = {
+    val Array(n, m) = StdIn.readLine.split(' ').map(_.toInt)
+
+    val netChicken = m - n
+
+    if (netChicken > 0)
+      println(s"Dr. Chaz will have $netChicken piece${if (1 < netChicken) "s" else ""} of chicken left over!")
+    else if (netChicken < 0)
+      println(s"Dr. Chaz needs ${-netChicken} more piece${if (netChicken < -1) "s" else ""} of chicken!")
+  }
+}
