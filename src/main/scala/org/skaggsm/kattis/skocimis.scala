64ef53f7a45332dc3874d51b70608d3c7bcf8106
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-06 11:32:11 -0500

    Solved skocimis

---
+++ src/main/scala/org/skaggsm/kattis/skocimis.scala
@@ -0,0 +1,16 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/6/2018.
+  */
+object skocimis {
+  def main(args: Array[String]): Unit = {
+    val Array(a, b, c) = StdIn.readLine.split(' ').map(_.toInt)
+
+    val largestGap = math.max(b - a, c - b)
+
+    println(largestGap - 1)
+  }
+}
