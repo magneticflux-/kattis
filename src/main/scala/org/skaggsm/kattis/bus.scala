385459d723e58639a2bf9ce4e91760f214bd45e8
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-06 21:55:33 -0500

    Solved bus

---
+++ src/main/scala/org/skaggsm/kattis/bus.scala
@@ -0,0 +1,24 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/6/2018.
+  */
+object bus {
+  def main(args: Array[String]): Unit = {
+    val cases = StdIn.readInt
+
+    Iterator.continually(StdIn.readInt)
+      .take(cases)
+      .map(i => {
+        (0 until i)
+          .map(_.toDouble)
+          .fold(0d)((a: Double, _) => {
+            (a + 0.5) * 2
+          })
+          .round
+      })
+      .foreach(println)
+  }
+}
