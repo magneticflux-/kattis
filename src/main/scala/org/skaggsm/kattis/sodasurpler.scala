88ce02dbea21e99202fb0f222787a488c573475f
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-06 21:17:36 -0500

    Solved sodasurpler

---
+++ src/main/scala/org/skaggsm/kattis/sodasurpler.scala
@@ -0,0 +1,24 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/6/2018.
+  */
+object sodasurpler {
+  def main(args: Array[String]): Unit = {
+    val Array(e, f, c) = StdIn.readLine.split(' ').map(_.toInt)
+
+    var bottles = e + f
+    var drinks = 0
+
+    while (bottles >= c) {
+      val newBottles = bottles / c
+      drinks += newBottles
+
+      bottles -= newBottles * (c - 1)
+    }
+
+    println(drinks)
+  }
+}
