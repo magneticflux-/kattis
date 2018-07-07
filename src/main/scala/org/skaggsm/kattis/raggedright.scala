0951f98383832d9ad5f66a3fc40b5e462f990c93
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-07 11:11:14 -0500

    Solved raggedright

---
+++ src/main/scala/org/skaggsm/kattis/raggedright.scala
@@ -0,0 +1,24 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/7/2018.
+  */
+object raggedright {
+  def main(args: Array[String]): Unit = {
+    val lines = Iterator.continually(StdIn.readLine)
+      .takeWhile(line => line != null && line.nonEmpty)
+      .toSeq
+
+    val maxLength = lines.map(_.length).max
+
+    val score = lines.init.map(line => {
+      val t = maxLength - line.length
+      t * t
+    }).sum
+
+    println(score)
+
+  }
+}
