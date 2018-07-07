e4adb04d6d8b167d7dd12708f94e52939f12ccd8
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-07 10:46:17 -0500

    Solved sok

---
+++ src/main/scala/org/skaggsm/kattis/sok.scala
@@ -0,0 +1,29 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/7/2018.
+  */
+object sok {
+  def main(args: Array[String]): Unit = {
+    val Array(a, b, c) = StdIn.readLine.split(' ').map(_.toInt)
+    val Array(i, j, k) = StdIn.readLine.split(' ').map(_.toInt)
+
+    val possibleAUsage = a / i.toDouble
+    val possibleBUsage = b / j.toDouble
+    val possibleCUsage = c / k.toDouble
+
+    val minUsage = Seq(possibleAUsage, possibleBUsage, possibleCUsage).min
+
+    val actualAUsage = i * minUsage
+    val actualBUsage = j * minUsage
+    val actualCUsage = k * minUsage
+
+    val aRemaining = a - actualAUsage
+    val bRemaining = b - actualBUsage
+    val cRemaining = c - actualCUsage
+
+    println(s"$aRemaining $bRemaining $cRemaining")
+  }
+}
