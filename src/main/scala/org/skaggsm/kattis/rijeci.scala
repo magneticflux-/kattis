ab80a7760bbfb48d6a55145fed380b96ce572580
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-07 21:00:19 -0500

    Solved rijeci

---
+++ src/main/scala/org/skaggsm/kattis/rijeci.scala
@@ -0,0 +1,31 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/7/2018.
+  */
+object rijeci {
+  def main(args: Array[String]): Unit = {
+    val k = StdIn.readInt
+
+    val finalState = Iterator.iterate(State(1, 0))(_.advance())
+      .take(k + 1)
+      .toSeq
+      .last
+
+    println(s"$finalState")
+  }
+
+  case class State(a: Int, b: Int) {
+    def advance(): State = {
+      val newA = b
+      val newB = b + a
+
+      copy(newA, newB)
+    }
+
+    override def toString: String = s"$a $b"
+  }
+
+}
