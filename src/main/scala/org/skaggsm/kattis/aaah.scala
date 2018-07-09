fced5388c9396e2394e5f1d402ccc06fd982a1ed
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-09 16:22:14 -0500

    Solved aaah

---
+++ src/main/scala/org/skaggsm/kattis/aaah.scala
@@ -0,0 +1,18 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/9/2018.
+  */
+object aaah {
+  def main(args: Array[String]): Unit = {
+    val possiblePower = StdIn.readLine.count(_ == 'a')
+    val requiredPower = StdIn.readLine.count(_ == 'a')
+
+    if (possiblePower >= requiredPower)
+      println("go")
+    else
+      println("no")
+  }
+}
