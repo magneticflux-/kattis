b7135ac7c1e22b6ed4fdfa51b8edf68a90f2c496
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-09 16:16:23 -0500

    Solved patuljci

---
+++ src/main/scala/org/skaggsm/kattis/patuljci.scala
@@ -0,0 +1,19 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/9/2018.
+  */
+object patuljci {
+  def main(args: Array[String]): Unit = {
+    val dwarves = Stream.continually(StdIn.readInt)
+      .take(9)
+
+    val solution = dwarves.combinations(7).find(_.sum == 100)
+
+    solution.transpose
+      .flatten
+      .foreach(println)
+  }
+}
