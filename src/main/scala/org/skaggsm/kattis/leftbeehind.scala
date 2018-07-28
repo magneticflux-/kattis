80b2781e4c0aef89d302da79d2269238aa177fcb
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-28 14:36:12 -0500

    Solved leftbeehind

---
+++ src/main/scala/org/skaggsm/kattis/leftbeehind.scala
@@ -0,0 +1,26 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+
+/**
+  * Created by Mitchell Skaggs on 7/28/2018.
+  */
+object leftbeehind {
+  def main(args: Array[String]): Unit = {
+    Stream.continually(StdIn.readLine)
+      .takeWhile(_ != "0 0")
+      .map(l => {
+        val Array(sweet, sour) = l.split(' ').map(_.toInt)
+
+        if (sweet + sour == 13)
+          "Never speak again."
+        else if (sweet > sour)
+          "To the convention."
+        else if (sweet < sour)
+          "Left beehind."
+        else
+          "Undecided."
+      })
+      .foreach(println)
+  }
+}
