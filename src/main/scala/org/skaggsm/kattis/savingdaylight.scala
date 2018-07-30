e950ea912a418e654e16458c15645395f994b33c
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-29 20:36:48 -0500

    Solved savingdaylight

---
+++ src/main/scala/org/skaggsm/kattis/savingdaylight.scala
@@ -0,0 +1,24 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+import scala.util.matching.Regex
+
+/**
+  * Created by Mitchell Skaggs on 7/29/2018.
+  */
+object savingdaylight {
+
+  val dateTimePattern: Regex = """(\w+ \d+ \d+) (\d+):(\d+) (\d+):(\d+)""".r
+
+  def main(args: Array[String]): Unit = {
+    Stream.continually(StdIn.readLine)
+      .takeWhile(s => s != null && s.nonEmpty)
+      .map {
+        case dateTimePattern(date, startH, startM, endH, endM) =>
+          val totalMinutes = (endH.toInt - startH.toInt) * 60 + (endM.toInt - startM.toInt)
+
+          s"$date ${totalMinutes / 60} hours ${totalMinutes % 60} minutes"
+      }
+      .foreach(println)
+  }
+}
