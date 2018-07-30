1a59a85d9dcc153d7196677bde0d21debf7e9fd9
Mitchell Skaggs <skaggsm333@gmail.com>
2018-07-29 20:40:21 -0500

    Solved areal

---
+++ src/main/scala/org/skaggsm/kattis/areal.scala
@@ -0,0 +1,17 @@
+package org.skaggsm.kattis
+
+import scala.io.StdIn
+import scala.math.sqrt
+
+/**
+  * Created by Mitchell Skaggs on 7/29/2018.
+  */
+object areal {
+  def main(args: Array[String]): Unit = {
+    val area = StdIn.readLong()
+    val sideLength = sqrt(area)
+    val perimeter = sideLength * 4
+
+    println(perimeter)
+  }
+}
