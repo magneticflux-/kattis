0e9e16392201d3e56e352bd5cdacf41cf13da408
Mitchell Skaggs <skaggsm333@gmail.com>
2022-05-02 00:42:20 -0500

    Add Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     scala
+    kotlin("jvm") version "1.6.21"
 }
 
 repositories {
@@ -8,4 +11,12 @@ repositories {
 
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.1.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.0")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        apiVersion = "1.3"
+        languageVersion = "1.3"
+    }
 }
